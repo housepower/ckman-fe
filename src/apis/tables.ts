77387322b60ebb49576c336011da0d60e7bc0c44
laura <957737702@qq.com>
2021-06-26 23:00:17 +0800

    feat: Table Metrics add Schema and Delete

--- src/apis/tables.ts
+++ src/apis/tables.ts
@@ -9,6 +9,16 @@ export const TablesApi = {
   tableMetrics(name: string) {
     return axios.get(`${url}/ck/table_metric/${name}`);
   },
+  deleteTable(name: string, params: { tableName: string; database: string }) {
+    return axios.delete(`${url}/ck/table/${name}`, {
+      params,
+    });
+  },
+  viewTableCreateSql(name: string, params: { tableName: string; database: string }) {
+    return axios.get(`${url}/ck/table_schema/${name}`, {
+      params,
+    });
+  },
   replicationStatus(name: string) {
     return axios.get(`${url}/zk/replicated_table/${name}`);
   },
---
+++ src/components/sql-code-mirror/sql-code-mirror.vue
@@ -0,0 +1,54 @@
+<template>
+  <textarea id="code" name="code" style="border: none;"></textarea>
+</template>
+<script>
+import CodeMirror from 'codemirror';
+import 'codemirror/addon/fold/brace-fold';
+import 'codemirror/addon/fold/foldgutter';
+import 'codemirror/addon/fold/foldgutter.css';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/darcula.css';
+import "codemirror/addon/hint/show-hint.css";
+import 'codemirror/mode/sql/sql';
+import 'codemirror/addon/hint/show-hint';
+import 'codemirror/addon/hint/sql-hint';
+import { format } from 'sql-formatter';
+
+export default {
+  name: 'SqlCodeMirror',
+  components: {
+    CodeMirror,
+  },
+
+  props: ["sql"],
+
+  data() {
+    return {
+      code: '',
+      sqlEditor: '',
+    }
+  },
+
+  created() {
+    this.code = format(this.sql);
+  },
+
+  mounted() {
+    const editor = this.sqlEditor = CodeMirror.fromTextArea(document.getElementById('code'), {
+      mode: 'sql',
+      theme: 'darcula',
+      indentWithTabs: true,
+      smartIndent: true,
+      lineNumbers: true,
+      matchBrackets : true,
+      autofocus: true,
+      readOnly: true,
+    });
+    editor.setValue(this.code);
+  },
+
+  onOk() {
+  }
+}
+</script>
