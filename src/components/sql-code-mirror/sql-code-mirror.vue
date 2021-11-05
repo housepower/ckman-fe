b3c475b1e1fdc0aea5146896717fc818c9e4003d
laura <957737702@qq.com>
2021-11-05 15:35:57 +0800

    sql编辑器支持clickhouse模式
    (cherry picked from commit 3fdad1ca68776ce8b3727be6e582528ca1e8e7f8)

--- src/components/sql-code-mirror/sql-code-mirror.vue
+++ src/components/sql-code-mirror/sql-code-mirror.vue
@@ -18,6 +18,25 @@ import 'codemirror/keymap/sublime';
 import { format } from 'sql-formatter';
 import { trim } from 'lodash-es';
 
+function set(str) {
+  let obj = {}, words = str.split(" ");
+  for (let i = 0; i < words.length; ++i) obj[words[i]] = true;
+  return obj;
+}
+var sqlKeywords = "alter and as asc between by count create delete desc distinct drop from group having in insert into is join like not on or order select set table union update values where limit ";
+// ClickHouse
+CodeMirror.defineMIME("text/x-clickhouse", {
+  name: "sql",
+  client: set(''),
+  keywords: set(sqlKeywords + "Int8 Int16 Int32 Int64 Int128 Int256 UInt8 UInt16 UInt32 UInt64 UInt128 UInt256 Float32 Float64 Decimal String FixedString UUID Date Date32 DateTime DateTime64 Enum LowCardinality Array AggregateFunction Nested Tuple Nullable SimpleAggregateFunction ALL ANY ASOF INNER LEFT RIGHT FULL CROSS OUTER SEMI ANTI JOIN USING WITH CUBE TOTALS HAVING ORDER BY FROM TO FILL STEP LIMIT ARRAY DISTINCT EXCEPT FORMAT INTO OUTFILE PREWHERE SAMPLE UNION FROM DATABASE TABLE VIEW MATERIALIZED DICTIONARY FUNCTION USER ROLE ROW POLICY QUOTA SETTINGS PROFILE ALTER COLUMN PARTITION DELETE UPDATE INDEX CONSTRANINT TTL USER QUOTA ROLE PROJECTION SYSTEM SHOW GRANT EXPLAIN REVOKE ATTACH DESCRIBE DETACH DROP EXISTS KILL OPTIMIZE RENAME EXCHANGE SET TRUNCATE USE WATCH ON IN CLUSTER ROLLUP SYNC NULL CAST MergeTree SummingMergeTree AggregatingMergeTree CollapsingMergeTree VersionedCollapsingMergeTree GraphiteMergeTree ReplacingMergeTRee ReplicatedMergeTree  ReplicatedSummingMergeTree ReplicatedReplacingMergeTree ReplicatedAggregatingMergeTree ReplicatedCollapsingMergeTree ReplicatedVersionedCollapsingMergeTree ReplicatedGraphiteMergeTree"),
+  builtin: set("count min max sum avg any stddevPop stddevSamp varPop varSamp covarPop covarSamp anyHeavy anyLast argMin argMax avgWeighted coor topK topKWeighted groupArray groupUniqArray groupArrayInsertAt groupArrayMovingSum groupArrayMOvingAvg groupArraySample groupBitAnd groupBitOr groupBitXor groupBitmap groupBitmapAnd groupBitmapOr groupBitmapXor sumWithOverflow deltaSum deltaSumTimestamp sumMap minMap maxMap sumCount rankCorr sumKahan intervalLengthSum skewPop skewSamp kurtPop kurtSamp uniq uniqExact uniqCombined uniqCombined64 uniqHLL12 uniqTheta quantile quantilesExactExclusive quantilesExactInclusive quantileExact quantileExactWeighted quantileTiming quantileTimingWeighted quantileDeterministic quantileTDigest quantileTDigestWeighted quantileBFloat16 median simpleLinearRegression stochasticLinearRegression stochasticLogisticRegression  categoricalInformationValue studentTTest welchTTest entropy mannWhitneyUTest histogram sequenceMatch sequenceCount windowFunnel retention uniqUpTo sumMapFiltered sequenceNextNode file merge numbers remote url mysql postgresql jdbc odbc hdfs s3 input generateRandom cluster view null dictionary s3Cluster sqlite"),
+  atoms: set(''),
+  operatorChars: /^[*+\-%<>!=&|^]/,
+  dateSQL: set("now"),
+  support: set("binaryNumber hexNumber doubleQuote"),
+  hooks: {}
+});
+
 export default {
   name: 'SqlCodeMirror',
   components: {
@@ -55,16 +74,9 @@ export default {
 
   mounted() {
     const editor = this.sqlEditor = CodeMirror.fromTextArea(this.$refs.code, {
-      mode: 'sql',
+      mode: 'text/x-clickhouse',
       theme: 'darcula',
-      // indentWithTabs: true,
-      // smartIndent: true,
-      // lineNumbers: true,
-      // matchBrackets : true,
-      // autofocus: true,
-      // readOnly: this.readOnly,
-      // height: 500,
-      // theme: 'default',
+      readOnly: this.readOnly,
       indentWithTabs: true,
       smartIndent: true,
       lineNumbers: true,
