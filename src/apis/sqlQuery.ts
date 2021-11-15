4e55e7850a186edd85124d88ce57e08604571dff
laura <957737702@qq.com>
2021-11-15 11:28:30 +0800

    修复大数显示不正确问题
    (cherry picked from commit e482d66a71395c91a3ffbda3377b232529453722)

--- src/apis/sqlQuery.ts
+++ src/apis/sqlQuery.ts
@@ -1,10 +1,19 @@
 import axios from 'axios';
-
+import JSONbig from 'json-big';
 const url = '/api/v1/ck';
 
 export const SqlQueryApi =  {
   query(params) {
-    return axios.get(`${url}/query/${params.clusterName}`, { params });
+    return axios.get(`${url}/query/${params.clusterName}`, {
+      params,
+      transformResponse: [data => {
+        try {
+          return JSONbig.parse(data);
+        } catch (err) {
+          return JSON.parse(data);
+        }
+      }],
+    });
   },
   getTableLists(clusterName) {
     return axios.get(`${url}/table_lists/${clusterName}`);
