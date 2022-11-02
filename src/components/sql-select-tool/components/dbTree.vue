0f2ec5a6cad1ad90ba2fa21fd0ffc9470207cdc2
laura <957737702@qq.com>
2022-11-02 11:25:57 +0800

    优化：数据库列表搜索卡顿问题

--- src/components/sql-select-tool/components/dbTree.vue
+++ src/components/sql-select-tool/components/dbTree.vue
@@ -4,70 +4,78 @@
       size="mini"
       :placeholder="$t('common.keyword search')"
       clearable
-      v-model="filterText">
+      v-model="filterText"
+      @change="onEnter">
     </el-input>
-
-    <el-tree
-      class="filter-tree flex-1 mt-5"
-      :data="data"
-      :props="defaultProps"
-      :filter-node-method="filterNode"
-      ref="tree">
-      <span class="custom-tree-node" slot-scope="{ node, data }">
-        <i :class="data.icon"></i><span class="ml-5">{{ node.label }}</span>
-      </span>
-    </el-tree>
+    <div ref="tree" id="ztree" class="ztree"></div>
   </div>
 </template>
 <script>
 import { SqlQueryApi } from '@/apis';
-export default {
-  watch: {
-    filterText(val) {
-      this.$refs.tree.filter(val);
-    }
-  },
 
+export default {
   data() {
     return {
       filterText: '',
-      data: [],
-      defaultProps: {
-        children: 'children',
-        label: 'label'
-      }
+      data: null,
+      loading: false,
+      setting: {
+        view: {
+          txtSelectedEnable: true,
+        }
+      },
     };
   },
 
-  created() {
-    this.getTableList();
+  async mounted() {
+    await this.getTableList();
+    const { setting, data } = this;
+    $.fn.zTree.init($('#ztree'), setting, data);
   },
 
   methods: {
-    // 对树过滤
-    filterNode(value, data) {
-      if (!value) return true;
-      return data.label.indexOf(value) !== -1;
+    onEnter() {
+      var zTree = $.fn.zTree.getZTreeObj("ztree");
+      const { filterText } = this;
+      const fn = (node) => node.name.indexOf(filterText) !== -1;
+      const showNodes = zTree.getNodesByFilter((node) => {
+        const children = zTree.getNodesByParamFuzzy('name', filterText, node);
+        return fn(node) || (children && children.length);
+      });
+      zTree.showNodes(showNodes);
+
+      const hideNodes = zTree.getNodesByFilter((node) => {
+        const children = zTree.getNodesByParamFuzzy('name', filterText, node);
+        return !fn(node) && !(children && children.length);
+      });
+      zTree.hideNodes(hideNodes);
     },
     // 获取数据库表结构数据，并转化成结构化树数据
     async getTableList() {
       const { id: clusterName } = this.$route.params;
-      const { data: { entity } } = await SqlQueryApi.getTableLists(clusterName);
+      this.loading = true;
+      const { data: { entity } } = await SqlQueryApi.getTableLists(clusterName)
+      .finally(() => this.loading = false);
       const treeData = (Object.keys(entity)||[]).map(dbName => {
         const db = {
           id: dbName,
-          label: dbName,
-          icon: 'fa fa-database',
+          name: dbName,
+          icon: '/images/database.png',
+          leaf: false,
+          expand: false,
           children: (Object.keys(entity[dbName])||[]).map(tableName => {
             const table = {
-              id: tableName,
-              label: tableName,
-              icon: 'fa fa-table',
+              id: dbName + '-' + tableName,
+              name: tableName,
+              icon: '/images/table.png',
+              leaf: false,
+              expand: false,
               children: (entity[dbName][tableName]||[]).map(columnName => {
                 return {
                   id: columnName,
-                  label: columnName,
-                  icon: 'fa fa-columns'
+                  name: columnName,
+                  icon: '/images/columns.png',
+                  leaf: true,
                 }
               })
             };
@@ -78,12 +86,28 @@ export default {
       });
 
       this.data = treeData;
-    }
+    },
   }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .filter-tree {
   overflow: auto;
+}
+.tree-component {
+  color: #606266;
+  overflow-y: auto;
+}
+</style>
+
+<style>
+.ztree * {
+  font-size: 14px;
+  color: #606266;
+  font-family: inherit;
+}
+span[treenode_ico] {
+  background-size: 12px 12px !important;
+  background-position: center center !important;
 }
 </style>
