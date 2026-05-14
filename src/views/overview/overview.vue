37fc28523afc1de964bd4eada5c3603971a13e1f
YenchangChan <chenyanchang1990@163.com>
2026-05-14 21:57:33 +0800

    feat(overview): hoist timeFilter state to page-level + PageHeader

--- src/views/overview/overview.vue
+++ src/views/overview/overview.vue
@@ -1,19 +1,48 @@
 <template>
-  <overview-base :metrics="metrics"
-                 :breadcrumbInfo="breadcrumbInfo" />
+  <div class="overview-page">
+    <PageHeader
+      :crumb="[$t('layout.ClickHouse Management Console'), clusterName, $t('home.Overview')]"
+      :title="clusterName"
+      :subtitle="$t('home.Overview')"
+    >
+      <template #actions>
+        <time-filter
+          v-model="timeFilter"
+          localKey="overviewBaseTimeFilter"
+          :refreshDuration.sync="refresh"
+        />
+      </template>
+    </PageHeader>
+    <overview-base
+      :metrics="metrics"
+      :time-filter="timeFilter"
+      :refresh-duration="refresh"
+    />
+  </div>
 </template>
+
 <script>
 import { Metrics } from "@/constants";
 
 export default {
+  name: 'Overview',
   data() {
     return {
       metrics: Metrics,
-      breadcrumbInfo: [],
+      timeFilter: ["now-1h", "now"],
+      refresh: null,
     };
   },
-  mounted() {
-    this.breadcrumbInfo = ["Clusters", this.$route.params.id, "overview"];
+  computed: {
+    clusterName() {
+      return this.$route.params.id || '';
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.overview-page {
+  padding-bottom: var(--s-8);
+}
+</style>
