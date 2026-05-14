778a2348d45b3a967e7cb1c5c0360a6797ff5ae3
YenchangChan <chenyanchang1990@163.com>
2026-05-14 18:05:26 +0800

    feat(breadcrumb): use chevron separator, token-based visuals

--- src/components/breadcrumb.vue
+++ src/components/breadcrumb.vue
@@ -1,46 +1,61 @@
 <template>
-  <section class="breadcrum mb-10 flex-vcenter flex-between plr-10">
-    <el-breadcrumb separator-class="el-icon-arrow-right">
+  <section class="breadcrum">
+    <el-breadcrumb separator="›">
       <el-breadcrumb-item>
         <router-link to="/"><i class="fa fa-home"></i></router-link>
       </el-breadcrumb-item>
-      <el-breadcrumb-item v-for="(item, index) of data"
-                          :key="index"
-                          :class="{ active: index === data.length - 1 }">{{index !== 1  ? $t('common.' + item) : item }}</el-breadcrumb-item>
+      <el-breadcrumb-item
+        v-for="(item, index) of data"
+        :key="index"
+        :class="{ active: index === data.length - 1 }"
+      >
+        {{ index !== 1 ? $t('common.' + item) : item }}
+      </el-breadcrumb-item>
     </el-breadcrumb>
     <div class="slot">
       <slot></slot>
     </div>
   </section>
 </template>
+
 <script>
 export default {
-  name: "Breadcrumb",
+  name: 'Breadcrumb',
   props: {
     data: {
       type: Array,
-      default: [],
+      default: () => [],
     },
   },
-  data() {
-    return {};
-  },
-  mounted() {},
 };
 </script>
 
 <style lang="scss" scoped>
 .breadcrum {
-  height: 45px;
-  background: #fcfcfc;
-  border: 1px solid #e8eaec;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 40px;
+  padding: 0 var(--s-3);
+  margin-bottom: var(--s-2);
+  background: var(--c-surface-0);
+  border: 1px solid var(--c-surface-3);
+  border-radius: var(--r-md);
 
   ::v-deep .el-breadcrumb__inner {
-    font-size: 18px;
+    font-size: var(--fs-sm);
+    color: var(--c-text-secondary);
   }
+
+  ::v-deep .el-breadcrumb__separator {
+    color: var(--c-text-tertiary);
+    margin: 0 var(--s-2);
+  }
+
   .active {
     ::v-deep .el-breadcrumb__inner {
-      color: var(--primary-color);
+      color: var(--c-text-primary);
+      font-weight: var(--fw-semibold);
     }
   }
 }
